7b92eb04b1d75ee1111274f21fa23796d47e8bbe
Sultan Ullah <atiqullah@Mac-Mini-2.local>
2022-12-23 17:31:13 -0800

    build and server running

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello World!");
+const express = require('express')
+const app = express()
+const port = 5001
+const path = require('path')
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
