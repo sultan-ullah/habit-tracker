ee6d3bcb60665da293271abeec6c6207d6116178
Sultan Ullah <atiqullah@Mac-Mini-5.local>
2023-04-26 00:47:02 -0700

    change to use typescrpt

---
+++ backend/index.ts
@@ -0,0 +1,24 @@
+/*import express, { json } from "express";
+/*const app = express();*/
+/*const port = process.env.PORT || 5001;*/
+/*import cors from "cors";*/
+
+/*import habitsRouter from "./habitsRouter";*/
+/*import usersRouter from "./usersRouter";*/
+/*import { config } from "dotenv";*/
+/*config();*/
+/*const corsOptions = {*/
+/*origin: "http://localhost:3001",*/
+/*};*/
+
+/*app.use(cors(corsOptions));*/
+/*app.use(json());*/
+/*app.use(habitsRouter);*/
+/*app.use(usersRouter);*/
+
+/*//TODO: generate index for user database*/
+
+/*app.listen(port, () => {*/
+/*console.log(`Example app listening on port ${port}`);*/
+
+/*}*/ //);
