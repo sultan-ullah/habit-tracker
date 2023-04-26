7f73f28db8f2ad3e600fb6f6a2a60a41a38befad
Sultan Ullah <atiqullah@Mac-Mini-5.local>
2023-04-26 00:58:12 -0700

    adding tsconfig and yarn.lock

--- backend/index.ts
+++ backend/index.ts
@@ -1,24 +1,23 @@
-/*import express, { json } from "express";
-/*const app = express();*/
-/*const port = process.env.PORT || 5001;*/
-/*import cors from "cors";*/
+import express, { json } from "express";
+const app = express();
+const port = process.env.PORT || 5001;
+import cors from "cors";
 
-/*import habitsRouter from "./habitsRouter";*/
-/*import usersRouter from "./usersRouter";*/
-/*import { config } from "dotenv";*/
-/*config();*/
-/*const corsOptions = {*/
-/*origin: "http://localhost:3001",*/
-/*};*/
+import habitsRouter from "./habitsRouter";
+import usersRouter from "./usersRouter";
+import { config } from "dotenv";
+config();
+const corsOptions = {
+  origin: "http://localhost:3001",
+};
 
-/*app.use(cors(corsOptions));*/
-/*app.use(json());*/
-/*app.use(habitsRouter);*/
-/*app.use(usersRouter);*/
+app.use(cors(corsOptions));
+app.use(json());
+app.use(habitsRouter);
+app.use(usersRouter);
 
-/*//TODO: generate index for user database*/
+//TODO: generate index for user database
 
-/*app.listen(port, () => {*/
-/*console.log(`Example app listening on port ${port}`);*/
-
-/*}*/ //);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
